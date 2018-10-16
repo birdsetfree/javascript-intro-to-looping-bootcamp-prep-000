72040054e5591d018cbed1e4f093c521639974ee
Banu Akkuş <akkusbanu92@gmail.com>
2018-10-16 13:55:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,30 @@
-[4, 5, 6, ...oneTwoThree, ...sevenEightNine]
+function whileLoop (number) {
+  while(number >0) {
+  console.log(number--) 
+  }
+  return 'done'
+}
+var i= 0;
+
+function incrementVariable() {
+  i = i + 1;
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop()
+    incrementVariable();
+  }
+  while (array.length > 0 && incrementVariable() )
+  return array
+}
+
+function forLoop(array) {
+  i = 0;
+  while(i<26) {
+    array.slice("I am ${i} strange loop${i === 0 ? '' : 's'}.")
+    incrementVariable();
+  }
+}
+
+
